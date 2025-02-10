7a4d6c77ffc72d0f629376e68c4f10fdde7af036
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2025-02-10 14:52:18 -0800

    Add files via upload

---
+++ include/system.h
@@ -0,0 +1,31 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+#include "include/mcemu.h"
+
+#define SYS_LOAD_MC_MODULES   0x01
+#define SYS_LOAD_USB_MODULES  0x02
+#define SYS_LOAD_ISOFS_MODULE 0x04
+
+#define NEUTRINO_PATH     "mc0:NEUTRINO/neutrino.elf"
+#define NEUTRINO_ALT_PATH "mc1:NEUTRINO/neutrino.elf"
+
+unsigned int USBA_crc32(const char *string);
+int sysGetDiscID(char *discID);
+void sysInitDev9(void);
+void sysShutdownDev9(void);
+void sysReset(int modload_mask);
+void sysExecExit(void);
+void sysPowerOff(void);
+#ifdef __DECI2_DEBUG
+int sysInitDECI2(void);
+#endif
+
+void sysLaunchLoaderElf(const char *filename, const char *mode_str, int size_cdvdman_irx, void **cdvdman_irx, int size_mcemu_irx, void **mcemu_irx, int EnablePS2Logo, unsigned int compatflags);
+void sysLaunchNeutrino(const char *driver, const char *path, int compatmask, int EnablePS2Logo, const char *neutrinoPath);
+
+int sysLoadModuleBuffer(void *buffer, int size, int argc, char *argv);
+int sysCheckMC(void);
+int sysCheckVMC(const char *prefix, const char *sep, char *name, int createSize, vmc_superblock_t *vmc_superblock);
+
+#endif
